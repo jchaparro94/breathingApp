3a2ee2ed803402f0825b16c4359fca1f4281dbb0
Jose <chaparro.jjr@gmail.com>
2020-06-07 22:14:15 -0700

    styling complete with animations

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const container = document.querySelector('.container');
-const text = document.querySelector('#text');
+const container = document.getElementById('container');
+const text = document.getElementById('text');
 
 const totalTime = 7500;
-const breathTime = (totalTime / 5) * 2;
+const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
 breathAnimation();
 
 function breathAnimation() {
-  text.innerHTML = 'Breath In';
-
+  text.innerText = 'Breathe In!';
   container.className = 'container grow';
 
   setTimeout(() => {
     text.innerText = 'Hold';
 
     setTimeout(() => {
-      text.innerText = 'Breath Out';
+      text.innerText = 'Breathe Out!';
       container.className = 'container shrink';
-    }, holdTime)
-  }, breathTime);
-};
+    }, holdTime);
+  }, breatheTime);
+}
 
 setInterval(breathAnimation, totalTime);
